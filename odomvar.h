4b5ea179ffb1426ed213057e70a7560ee0fe9425
RoboCode <umkc@robocode2.(none)>
2012-10-25 09:55:15 -0500

    Create structs for acc-3d and 2d, and vel 2d

---
+++ odomvar.h
@@ -0,0 +1,23 @@
+struct acc3d
+{
+double x;
+double y;
+double z;
+double t; //theta
+double p; //phi
+double g; //gamma
+}
+
+struct acc2d
+{
+double x;
+double y;
+double t;
+}
+
+struct vel2d
+{
+double x;
+double y;
+double t;
+}
