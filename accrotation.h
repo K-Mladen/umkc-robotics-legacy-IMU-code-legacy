c4b3e35f2c05bba6987cc2cf4029f83100c7907b
K-Mladen <MK8907@gmail.com>
2012-11-09 15:22:17 -0600

    MK:Accrotation complete, except optimizations to reduce error.

--- accrotation.h
+++ accrotation.h
@@ -4,13 +4,39 @@
 #define accrotation
 
 #include "acc.h"
+#include <math>
 
 
-accfield accrotation(accphidget,adjust)
+accfield accrotation(accphidget,offset, posangle)
 {
 //get compass vector from compass or gyroscope --or passed in
-//translate accphidget to accrob, using adjust --these are structs; from acc.h maybe
-//get magnitude and {unit components if unit components are necessary}
+//need posangle.magnitude and posangle.unitx=posangle.x/posangle.magnitude
+
+//translate accphidget to accrob, using offset --these are structs; from acc.h maybe
+accrob.x = accphidget.x - sqrt(pow(accphidget.roty*offset.z,2) +
+								pow(accphidget.rotz*offset.y,2));
+accrob.y = accphidget.y - sqrt(pow(accphidget.rotx*offset.z,2) +
+								pow(accphidget.rotz*offset.x,2));
+accrob.z = accphidget.z - sqrt(pow(accphidget.roty*offset.x,2) +
+								pow(accphidget.rotx*offset.y,2));
+
+
+//calculate intermediate values
+
+//introduce rotation matrix
+//[1-2(c2+d2)	2(bc-ad)	2(ac+bd)	]	[old.x]		[new.x]
+//|2(ad+bc)		1-2(b2+d2)	2(cd-ab)	|	|old.y|	=	|new.y|
+//[2(bc-ad)		2(cd+ab)	1-2(b2+c2)	]	[old.z]		[new.z]
+
+accfield.x = (1-2*(c2+d2))*accrob.x + 2*(bc-ad)*accrob.y + 2*(ac+bd)*accrob.z;
+accfield.y = 2*(ad+bc)*accrob.x + (1-2*(b2+d2))*accrob.y + 2*(cd-ab)*accrob.z;
+//not needed: accfield.z
+//accfield.z = 2*(bd-ac)*accrob.x + 2*(cd+ab)*accrob.y + (1-2*(b2+c2))*accrob.z;
+
+//next, do the same for the angular accelerations
+
+
+
 //notation note:magnitude = t, cos = c, sin = s, unit components=ax,ay,az
 /* These don't quite work... for example, ^ isn't an operator. but I guess this is what a more optimized matrix
 looks like
@@ -19,6 +45,6 @@ accglobal.y = (ay*ax*(1-c(t))+az*s(t))*accrob.x + (c(t)+(ay^2)(1-c(t)))*accrob.y
 accglobal.z = (az*ax*(1-c(t))-ay*s(t))*accrob.x + (az*ay*(1-c(t))+ax*s(t))*accrob.y + (c(t)+(az^2)(1-c(t))*accrob.z;
 */
 
-return accglobal;
+return accfield;
 }
 #endif
