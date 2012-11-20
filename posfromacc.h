e0199dd18f3848988ef826d07dd8f6fb9f7a324c
K-Mladen <MK8907@gmail.com>
2012-11-19 19:23:51 -0600

    Saving changes that got lost I think...

--- posfromacc.h
+++ posfromacc.h
@@ -2,36 +2,37 @@
 *  This code will find the position based on acceleration data using the 	   *
 *	trapezoidal approximation of an integral.								   *
 *******************************************************************************/
-//TODO overload addition operator for cartesianCoordData in odomvar.h!
 
 #include "odomvar.h"
 
 cartesianCoordData posFromAcc(accField,posField,timePhidget)
 {
 	//declare objects
-	cartesianCoordData posFieldNew(0,0,0,0,0,0), velField(0,0,0,0,0,0);
+	cartesianCoordData posFieldNew(0,0,0,0,0,0), velField(0,0,0,0,0,0),
+						dvelField(0,0,0,0,0,0), dposFieldNew (0,0,0,0,0,0);
 	static cartesianCoordData accFieldPrevious(0,0,0,0,0,0),
 								velFieldPrevious(0,0,0,0,0,0);
 	static double timePhidgetPrevious = 0;
 	double timeElapsed = timePhidget - timePhidgetPrevious;
 
 	//math -- acceleration to velocity: Intermediate values ie change in vel.
-	velField.x = timeElapsed*(accFieldPrevious.x + accField.x)/2;
-	velField.y = timeElapsed*(accFieldPrevious.y + accField.y)/2;
-	velField.z = timeElapsed*(accFieldPrevious.z + accField.z)/2;
-	velField.rotx = timeElapsed*(accFieldPrevious.rotx + accField.rotx)/2;
-	velField.roty = timeElapsed*(accFieldPrevious.roty + accField.roty)/2;
-	velField.roty = timeElapsed*(accFieldPrevious.rotz + accField.rotz)/2;	
-	velField+=velFieldPrevious; //add vel pre-change to get current velocity.
+	dvelField.x = timeElapsed*(accFieldPrevious.x + accField.x)/2;
+	dvelField.y = timeElapsed*(accFieldPrevious.y + accField.y)/2;
+	dvelField.z = timeElapsed*(accFieldPrevious.z + accField.z)/2;
+	dvelField.rotx = timeElapsed*(accFieldPrevious.rotx + accField.rotx)/2;
+	dvelField.roty = timeElapsed*(accFieldPrevious.roty + accField.roty)/2;
+	dvelField.roty = timeElapsed*(accFieldPrevious.rotz + accField.rotz)/2;	
+	//add dvelField to velField
+	velField = dvelField.addComponents(velFieldPrevious);
 
 	//math -- velocity to position: Intermediate values ie change in pos.
-	posFieldNew.x = timeElapsed*(velFieldPrevious.x + velField.x)/2;
-	posFieldNew.y = timeElapsed*(velFieldPrevious.y + velField.y)/2;
-	posFieldNew.z = timeElapsed*(velFieldPrevious.z + velField.z)/2;
-	posFieldNew.rotx = timeElapsed*(velFieldPrevious.rotx + velField.rotx)/2;
-	posFieldNew.roty = timeElapsed*(velFieldPrevious.roty + velField.roty)/2;
-	posFieldNew.rotz = timeElapsed*(velFieldPrevious.rotz + velField.rotz)/2;
-	posFieldNew+=posField; //add pre-change position to get final position.
+	dposFieldNew.x = timeElapsed*(velFieldPrevious.x + velField.x)/2;
+	dposFieldNew.y = timeElapsed*(velFieldPrevious.y + velField.y)/2;
+	dposFieldNew.z = timeElapsed*(velFieldPrevious.z + velField.z)/2;
+	dposFieldNew.rotx = timeElapsed*(velFieldPrevious.rotx + velField.rotx)/2;
+	dposFieldNew.roty = timeElapsed*(velFieldPrevious.roty + velField.roty)/2;
+	dposFieldNew.rotz = timeElapsed*(velFieldPrevious.rotz + velField.rotz)/2;
+	posFieldNew = dposFieldNew.addComponents(posField)
 
 	//set "previous" values to "current" to prepare for next iteration
 	accFieldPrevious = accField;
