bdf36d0dc077aac70c78d19b8dfa3df7740e07e7
K-Mladen <MK8907@gmail.com>
2012-11-30 11:55:08 -0600

    Integration function... each datapt needs summing to calc integral

---
+++ simpsonIteration.cpp
@@ -0,0 +1,26 @@
+/*******************************************************************************
+*  Generates a datapoint, keeping a running total of these datapoints          **   implements simpsons rule
+*
+*******************************************************************************/
+
+
+double simpsonIteration(double pt0, double pt1, double pt2,
+                         int ust0, int ust1, int ust2)
+{
+  /*microseconds to seconds*/
+  double t0 = ust0/1000000,
+         t1 = ust1/1000000,
+         t2 = ust2/1000000;
+  /*make sure time interval is constant*/
+  if ((t1 - t0)=(t2 - t1))
+  {
+    /*if so, do simpsons rule normally*/
+    return ((t1-t0)/3*(pt0 + 4*pt1 + pt2));
+  } else {
+    /*Otherwise, split simpsons rule into smaller parts, and add them*/
+    double result0, result1;
+	result0 = (t1-t0)/3*(pt0+2*pt1);
+	result1 = (t2-t1)/3*(2*pt1+pt2);
+	return (result0+result1);
+  }
+}
