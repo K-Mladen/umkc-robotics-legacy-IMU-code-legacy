42adae385af254ace480d6c0566baa8db368308b
Robotics <robotics@robotlab-dev1.(none)>
2013-01-05 14:13:59 -0600

    ifndef

--- headerFiles/mathy.h
+++ headerFiles/mathy.h
@@ -1,6 +1,10 @@
+#ifndef mathy
+#define mathy
+
 double simpsonIteration(double pt0, double pt1, double pt2,
     int ust0, int ust1, int ust2);
 pVector rotatePOV(pVector vec, pVector rot);
 pVector orientation(pVector angle);
 pVector filter(pVector accel, pVector gyrot, double alpha);
 
+#endif
