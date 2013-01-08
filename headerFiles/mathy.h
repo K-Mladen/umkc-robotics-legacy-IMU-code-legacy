4b93e73c30c9826c9dcbeec90af5ff114e5d94b9
K-Mladen <mk8907@gmail.com>
2013-01-08 17:07:38 -0600

    fixed github's glitchyness -- consts magically reappearing

--- headerFiles/mathy.h
+++ headerFiles/mathy.h
@@ -11,8 +11,8 @@ namespace mathy	{
 
 double simpsonIteration(double const pt[], int const ust[]);
 void integrateGyro(spatial::PVectorQ* data, pVector& current);
-pVector rotatePOV(pVector const & vec, pVector const & rot);
-pVector orientation(pVector const & angle);
-pVector filter(pVector const & accel, pVector const & gyrot, double alpha);
+pVector rotatePOV(pVector & vec, pVector & rot);
+pVector orientation(pVector & angle);
+pVector filter(pVector & accel, pVector & gyrot, double alpha);
 
 #endif
