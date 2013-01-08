5a576e8afc8787251c50d0b1541a57148cfd763a
Robotics <robotics@robotlab-dev1.(none)>
2013-01-08 15:21:03 -0600

    pass all classes/structs by const reference

--- headerFiles/mathy.cpp
+++ headerFiles/mathy.cpp
@@ -18,7 +18,7 @@
 
 //pt[i] is the value of the ith element
 //ust[i] is the time (in us) at which the ith element was sampled
-double simpsonIteration(double pt[],ust[])
+double simpsonIteration(double const pt[],int const ust[])
 {
   /*microseconds to seconds*/
   double t0 = ust[0]/1000000.0,
@@ -39,7 +39,7 @@ double simpsonIteration(double pt[],ust[])
   }
 }
 
-void integrateGyro(spatial::PVectorQ* data, pVector& current)
+void integrateGyro(spatial::PVectorQ* const data, pVector& current)
 {
   pVector delta;
   double gyros[3]{data->at(0).angularRate,data->at(1).angularRate, data->at(2).angularRate}, 
@@ -62,14 +62,14 @@ void integrateGyro(spatial::PVectorQ* data, pVector& current)
 //changing-> rot is the current orientation vector??
 
 //changing this to a set method? to change contents of vec, instead of returning new pVector?
-pVector rotatePOV(pVector vec, pVector rot)
+pVector rotatePOV(pVector const & vec, pVector const & rot)
 {
 
   double rad =  std::atan(1)/45; //(pi/4)/45 = pi/180
   double s = sin(rot.magnitude()*rad),
          c = cos(rot.magnitude()*rad);
   
-  /*double xDir = (c+pow(rot.unitComponent(X),2)*(1-c))*vec.component(X) +
+  double xDir = (c+pow(rot.unitComponent(X),2)*(1-c))*vec.component(X) +
         (rot.unitComponent(X)*rot.unitComponent(Y)*(1-c)
         - rot.unitComponent(Z)*s)*vec.component(Y) +
         (rot.unitComponent(X)*rot.unitComponent(Z)*(1-c)
@@ -87,11 +87,9 @@ pVector rotatePOV(pVector vec, pVector rot)
           + rot.unitComponent(X)*s)*vec.component(Y) +
           (c+pow(rot.unitComponent(Z),2)*(1-c))*vec.component(Z);
 
-  vec.set(X, xDir);
-  vec.set(Y, yDir);
-  vec.set(Z, zDir);
-  */
-  pVector newVec
+  return vec.set(xDir, yDir, zDir);
+
+/*  pVector newVec
     (
 	//X component
 	(c+pow(rot.unitComponent(X),2)*(1-c))*vec.component(X) +
@@ -114,10 +112,11 @@ pVector rotatePOV(pVector vec, pVector rot)
 	);
 
    return newVec;
+*/
 }
 
 
-pVector orientation(pVector angle)
+pVector orientation(pVector const & angle)
 {
   //note+ need to change to using the constants in mathy.h 
 
@@ -127,7 +126,7 @@ pVector orientation(pVector angle)
 }
 
 
-pVector filter(pVector accel, pVector gyrot, double alpha)
+pVector filter(pVector const & accel, pVector const & gyrot, double alpha)
 {
   /*current time step t2 (so T=t-1 is t1 and T=t-2 is t0)*/
   pVector angle
