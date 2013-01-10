e9058d084ca83457373065d6308caa1df1cbd5dd
vptarmigan <vicky.ptarmigan@gmail.com>
2013-01-09 18:16:51 -0600

    Added in gyro offset constants

--- headerFiles/config.h
+++ headerFiles/config.h
@@ -13,17 +13,21 @@
 #endif
 
 #ifndef DEBUG_FILTERING
-#define DEBUG_FILTERING
+//#define DEBUG_FILTERING
 #endif
 
 #ifndef DEBUG_INTEGRATE
-#define DEBUG_INTEGRATE
+//#define DEBUG_INTEGRATE
 #endif
 
 //DEBUG
 #endif	
 
 const double US_PER_SECOND = 1000000;
+const int dataRate = 16;
+const double TAU = 20000;
+
+const double GYRO_OFFSET[3] = {-0.283139, -0.0655911. -0.340913};
 
 
 #endif
