c076dd6677aa89dfd7eaf17e4ed737279cdd25a2
vptarmigan <vicky.ptarmigan@gmail.com>
2013-01-12 13:11:02 -0600

    Added comments to config

--- headerFiles/config.h
+++ headerFiles/config.h
@@ -1,10 +1,14 @@
+
 #ifndef CONFIG_H
 #define CONFIG_H
+
+//========================================
+//DEBUGS
+//========================================
 
 #ifndef DEBUG
 #define DEBUG  //remove this line when done debugging
 #endif
-
 
 #ifdef DEBUG
 
@@ -20,21 +24,28 @@
 	//#define DEBUG_FILTERING
 	#endif
 
-	#ifndef DEBUG_LIVE_GRAPH_PHIDGET_RAW
+	//"@ Raw Phidget data, non zeroed, avg constant, then zeroed." 
+	#ifndef DEBUG_LIVE_GRAPH_PHIDGET_RAW	
 	#define DEBUG_LIVE_GRAPH_PHIDGET_RAW
 	#endif
 
+	//"@ Rotated gyro data, after zeroing
 	#ifndef DEBUG_LIVE_GRAPH_ROTATION
 	#define DEBUG_LIVE_GRAPH_ROTATION 
 	#endif
 
-
+	//"@ Current Orientation, rotated, then non filtered and filtered."
 	#ifndef DEBUG_LIVE_GRAPH_CURRENT_ORIENTATION
 	#define DEBUG_LIVE_GRAPH_CURRENT_ORIENTATION
 	#endif
 
 //DEBUG
 #endif	
+
+
+//========================================
+//CONSTANTS
+//========================================
 
 const double US_PER_SECOND = 1000000;
 const int dataRate = 16;
@@ -43,4 +54,5 @@ const double TAU = 20000;
 const double GYRO_OFFSET[3] = {-0.225902, 0.0424094, -0.25949};
 
 
+//CONFIG_H
 #endif
